1a3e3a1910e0356ec240402a510bf568612c1d5b
cmetankys <113020367+cmetankys@users.noreply.github.com>
2023-01-20 21:23:43 +0200

    Add files via upload

---
+++ 8_laba.cpp
@@ -0,0 +1,24 @@
+﻿/*8 лабораторная работа 7 вариант
+Дано целое число типа int, выведите на экран содержимое каждого из его байтов, используя знания по указателям. Дайте обоснование полученному результату.*/
+#include <iostream>
+#include <string>
+#include <cstdio>
+std::string getBinnary(unsigned char n) {
+	std::string buff;
+	while (n > 0) {
+		buff += std::to_string(n % 2);
+		n /= 2;
+	}
+	return std::string(buff.crbegin(), buff.crend());
+}
+int main() {
+	setlocale(LC_ALL, "ru");
+	std::cout << "Введите число типа int: ";
+	int value;
+	std::cin >> value;
+	unsigned char* value_p{ ((unsigned char*)&value) };
+	for (size_t i{}; i < sizeof(int); ++i, ++value_p) {
+		std::string binValue{ getBinnary(*value_p) };
+		printf("%p: %s\n", value_p, binValue.c_str());
+	}
+}
